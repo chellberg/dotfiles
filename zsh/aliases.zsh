4754e8e946e3672c556af446c90c40a83452f83e
Corin Chellberg <corinchellberg@gmail.com>
2015-12-04 16:00:58 -0500

    add zsh config and aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,3 +1,25 @@
-alias reload!='. ~/.zshrc'
+# HealthPro specific
 
-alias gorms='cd ~/Programming/healthpro/rms'
+alias rmbjs='rm ~/rms/app/assets/javascripts/bundle.js'
+alias rmorig='rm **/*.orig'
+alias prodc='source /Users/Corin/rms/bin/catalyze.sh -E healthproprod console app01 "bundle exec rails console"'
+alias stagingc='source /Users/Corin/rms/bin/catalyze.sh -E healthprostaging console app01 "bundle exec rails console"'
+alias dw='mux start rms'
+alias dwo='mux start onboarding'
+alias rl='bundle exec rake db:restore_prod_backup && rake db:migrate'
+alias cdr='cd ~/rms'
+
+# -------------------------------------------------------------------
+# OTHER aliases
+# -------------------------------------------------------------------
+ 
+alias reload="source ~/.zshrc"
+alias cl='clear'
+alias vim='mvim -v'
+alias vi='mvim -v'
+alias -g G='|grep '
+alias -s html=vim
+alias -s erb=vim
+alias -s rb=vim
+
+alias tks='tmux kill-server'
--- zsh/config.zsh
+++ zsh/config.zsh
@@ -1,8 +1,4 @@
-if [[ -n $SSH_CONNECTION ]]; then
-  export PS1='%m:%3~$(git_info_for_prompt)%# '
-else
-  export PS1='%3~$(git_info_for_prompt)%# '
-fi
+eval "$(rbenv init -)"
 
 export LSCOLORS="exfxcxdxbxegedabagacad"
 export CLICOLOR=true
@@ -15,13 +11,9 @@ HISTFILE=~/.zsh_history
 HISTSIZE=10000
 SAVEHIST=10000
 
-setopt NO_BG_NICE # don't nice background tasks
 setopt NO_HUP
 setopt NO_LIST_BEEP
-setopt LOCAL_OPTIONS # allow functions to have local options
-setopt LOCAL_TRAPS # allow functions to have local traps
 setopt HIST_VERIFY
-setopt SHARE_HISTORY # share history between sessions ???
 setopt EXTENDED_HISTORY # add timestamps to history
 setopt PROMPT_SUBST
 setopt CORRECT
@@ -42,10 +34,44 @@ setopt HIST_REDUCE_BLANKS
 
 zle -N newtab
 
-bindkey '^[^[[D' backward-word
-bindkey '^[^[[C' forward-word
-bindkey '^[[5D' beginning-of-line
-bindkey '^[[5C' end-of-line
-bindkey '^[[3~' delete-char
-bindkey '^[^N' newtab
-bindkey '^?' backward-delete-char
+# bindkey '^[^[[D' backward-word
+# bindkey '^[^[[C' forward-word
+# bindkey '^[[5D' beginning-of-line
+# bindkey '^[[5C' end-of-line
+# bindkey '^[[3~' delete-char
+# bindkey '^[^N' newtab
+# bindkey '^?' backward-delete-char
+
+plugins=(history-substring-search git tmux)
+
+DISABLE_CORRECTION="true"
+DISABLE_AUTO_UPDATE="true"
+
+bindkey "^[[A" up-line-or-beginning-search 
+bindkey "^[[B" down-line-or-beginning-search 
+
+source $ZSH/oh-my-zsh.sh
+source $HOME/.profile
+
+bindkey -M vicmd 'k' history-substring-search-up
+bindkey -M vicmd 'j' history-substring-search-down
+autoload -U up-line-or-beginning-search
+autoload -U down-line-or-beginning-search
+zle -N up-line-or-beginning-search
+zle -N down-line-or-beginning-search
+
+bindkey -v # vim mode in zsh
+
+function install_jsctags {
+  npm install jsctags
+  # https://github.com/mozilla/doctorjs/issues/52
+  gsed -i '51i tags: [],' ./node_modules/jsctags/jsctags/ctags/index.js
+}
+
+source $(brew --prefix nvm)/nvm.sh
+
+export DISABLE_AUTO_TITLE=true
+export PATH="/usr/local/sbin:/usr/local/bin:/Users/Corin/conf:$PATH"
+export NVM_DIR=~/.nvm
+# export ZSH_THEME="miloshadzic"
+export PGDATA="/usr/local/var/postgres"
