81bd7cfbb9a6cf8c2007c2dd6e69006265f8f355
Corin Chellberg <corinchellberg@gmail.com>
2017-02-13 13:17:56 -0500

    tweak grc stuff so it works

--- system/aliases.zsh
+++ system/aliases.zsh
@@ -3,9 +3,9 @@
 #   `brew install coreutils`
 if $(gls &>/dev/null)
 then
-  alias ls='gls -F --color'
-  alias l='gls -lAh --color'
-  alias ll='gls -l --color'
-  alias la='gls -A --color'
-  alias lla='gls -Al --color'
+  alias ls='grc ls -F --color'
+  alias l='ls -lAh --color'
+  alias ll='ls -l --color'
+  alias la='ls -A --color'
+  alias lla='ls -Al --color'
 fi
